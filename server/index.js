1605a708d2ed5659a4d03b05aa490ffcc2c8f6d5
vaishali goyal <vishug654@gmail.com>
2025-10-25 00:19:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,9 +27,10 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 // For any route not handled by API, serve React's index.html
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
+
 
 // Start cron job
 startSyncJob();
